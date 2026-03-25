8f4c36f5d17c7b235ecf34cb338bc66cee680c33
Gian Lazzarini <gianlazzarini@gmail.com>
2026-03-24 17:02:18 -0700

    precommit

--- public/sw.js
+++ public/sw.js
@@ -2631,7 +2631,7 @@ This is generally NOT safe. Learn more at https://bit.ly/wb-precache`;
 
   // views/assets/src-sw.ts
   clientsClaim();
-  precacheAndRoute([{"revision":"e4cb185b2e80d1080505613a40f431fc","url":"assets/lazztech_icon.png"},{"revision":"f0598a10b39d3b5a995d9ef85ede09c4","url":"assets/lazztech_icon.webp"},{"revision":"6f9ff248bde231042902e2b2088fe0b9","url":"bundle.css"},{"revision":"0609c12e87f501b8192828ad3f74c84f","url":"favicon.ico"},{"revision":"67f0c6927b568b4c2d310fbe10f10f70","url":"js/webPush.js"},{"revision":"dd284d5fd9d141740063d324844de705","url":"manifest.json"},{"revision":"6c54723b981c892a39e18e81e6346bdf","url":"robots.txt"}]);
+  precacheAndRoute([{"revision":"e4cb185b2e80d1080505613a40f431fc","url":"assets/lazztech_icon.png"},{"revision":"f0598a10b39d3b5a995d9ef85ede09c4","url":"assets/lazztech_icon.webp"},{"revision":"5d0f9dff3b5f6729b48b48bde0f55df5","url":"bundle.css"},{"revision":"0609c12e87f501b8192828ad3f74c84f","url":"favicon.ico"},{"revision":"67f0c6927b568b4c2d310fbe10f10f70","url":"js/webPush.js"},{"revision":"dd284d5fd9d141740063d324844de705","url":"manifest.json"},{"revision":"6c54723b981c892a39e18e81e6346bdf","url":"robots.txt"}]);
   var CACHE_STRATEGY = new NetworkFirst();
   var FALLBACK_HTML_URL = "/offline.html";
   warmStrategyCache({
