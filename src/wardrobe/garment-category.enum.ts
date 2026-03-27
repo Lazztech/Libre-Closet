feb281d1fac941bc9c6606c4d149a5d7e5d029c1
Gian Lazzarini <gianlazzarini@gmail.com>
2026-03-27 16:44:22 -0700

    Simplify garment categories and order

--- src/wardrobe/garment-category.enum.ts
+++ src/wardrobe/garment-category.enum.ts
@@ -1,12 +1,10 @@
 export enum GarmentCategory {
+  ACCESSORIES = 'accessories',
+  BAGS = 'bags',
   OUTERWEAR = 'outerwear',
   DRESSES = 'dresses',
   TOPS = 'tops',
-  LINGERIE = 'lingerie',
-  UNDERWEAR = 'underwear',
   BOTTOMS = 'bottoms',
   FOOTWEAR = 'footwear',
-  ACCESSORIES = 'accessories',
-  BAGS = 'bags',
   OTHER = 'other',
 }
