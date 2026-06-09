6891e8af5c22b872580c1021174635ae68a7a8f9
Gian Lazzarini <gianlazzarini@gmail.com>
2026-06-08 23:34:30 -0700

    fix: add AuthModule import to WardrobeShareModule for guard dependencies
    The WardrobeShareModule was missing AuthModule import, which caused
    ConditionalAuthGuard to fail at runtime since AuthService wasn't
    available in the module's dependency context.

--- src/wardrobe-share/wardrobe-share.module.ts
+++ src/wardrobe-share/wardrobe-share.module.ts
@@ -2,11 +2,12 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { WardrobeShare } from '../dal/entity/wardrobe-share.entity';
 import { User } from '../dal/entity/user.entity';
+import { AuthModule } from '../auth/auth.module';
 import { WardrobeShareController } from './wardrobe-share.controller';
 import { WardrobeShareService } from './wardrobe-share.service';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([WardrobeShare, User])],
+  imports: [MikroOrmModule.forFeature([WardrobeShare, User]), AuthModule],
   controllers: [WardrobeShareController],
   providers: [WardrobeShareService],
   exports: [WardrobeShareService],
